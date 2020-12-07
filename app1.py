3331abdad5bc6a83236e3c44c4706fed2a5aa72d
VladimirNikel <vova_niemisto@mail.ru>
2020-12-07 19:22:35 +0300

    Скелет первой программки

---
+++ app1.py
@@ -0,0 +1,35 @@
+import json
+import datetime
+
+print("*"*50,"\nНазвание: Посылатель запросов 3000.\nЭта программа умеет слать сообщение в общий топик Kafka.\nДля этого Вам будет предложено выбрать два имеющихся типа сообщений, но и также, предоставляется возможность ввести свой тип сообщений и отправить его в топик.\nАвтор: Ниемисто Владимир, Nikel 2020")
+print("*"*50)
+
+try:
+	while 1:
+		send_json = None
+		try:
+			send_type_message = int(input("\nКакой тип сообщения сейчас отправим?\n\t1 - 'message'\n\t2 - 'error'\n"))
+		except ValueError:
+			continue
+
+		if send_type_message == 1:
+			print(datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S"),"\tОкей, пошлю сообщение типа *message*")
+			send_json = json.dumps({"type":"message"})
+
+		else:
+			if send_type_message == 2:
+				print(datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S"),"\tОкей, пошлю сообщение типа *error*")
+				send_json = json.dumps({"type":"error"})
+
+			else:
+				other_type_message = str(input("Вероятно, Вы хотите ввести свой тип сообщения? Введите его сейчас: "))
+				print(datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S"),"\tОкей, тогда я отправлю сообщение типа *", other_type_message,"*")
+				send_json = json.dumps({"type":other_type_message})
+
+		print(json.loads(send_json))
+		
+except KeyboardInterrupt:
+	print("\n")
+	print("*"*50,"\nДо встречи, бро!\nNikel, 2020 ")
+
+
