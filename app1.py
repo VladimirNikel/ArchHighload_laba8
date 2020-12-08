f5d5ec2a72f400a3d9689d0813c38646f68561d4
VladimirNikel <vova_niemisto@mail.ru>
2020-12-08 13:53:27 +0300

    Попытка выгрузить на сервер

--- app1.py
+++ app1.py
@@ -1,5 +1,8 @@
 import json
 import datetime
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers=['194.61.2.84:5099'])				# value_serializer=lambda v: json.dumps(v).encode('utf-8')
 
 print("*"*50,"\nНазвание: Посылатель запросов 3000.\nЭта программа умеет слать сообщение в общий топик Kafka.\nДля этого Вам будет предложено выбрать два имеющихся типа сообщений, но и также, предоставляется возможность ввести свой тип сообщений и отправить его в топик.\nАвтор: Ниемисто Владимир, Nikel 2020")
 print("*"*50)
@@ -14,19 +17,20 @@ try:
 
 		if send_type_message == 1:
 			print(datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S"),"\tОкей, пошлю сообщение типа *message*")
-			send_json = json.dumps({"type":"message"})
-
+			send_json = (json.dumps({"type": "message"})).encode('utf-8')
+			
 		else:
 			if send_type_message == 2:
 				print(datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S"),"\tОкей, пошлю сообщение типа *error*")
-				send_json = json.dumps({"type":"error"})
+				send_json = (json.dumps({"type": "error"})).encode('utf-8')
 
 			else:
 				other_type_message = str(input("Вероятно, Вы хотите ввести свой тип сообщения? Введите его сейчас: "))
 				print(datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S"),"\tОкей, тогда я отправлю сообщение типа *", other_type_message,"*")
-				send_json = json.dumps({"type":other_type_message})
+				send_json = (json.dumps({"type": other_type_message})).encode('utf-8')
 
-		print(json.loads(send_json))
+		print(json.loads(send_json.decode('utf-8')))
+		producer.send('main_topic', send_json)
 		
 except KeyboardInterrupt:
 	print("\n")
--- app2.py
+++ app2.py
@@ -2,12 +2,16 @@ import json
 import datetime
 import random
 import time
+from kafka import KafkaConsumer
+
+consumer = KafkaConsumer('main_topic', group_id='test-consumer-group', bootstrap_servers=['194.61.2.84:5099'], api_version=(0,10,1), auto_offset_reset='earliest')
 
 print("*"*50,"\nНазвание: Приниматель запросов 3000.\nЭта программа умеет принимать сообщение из общего топика Kafka.\nЕсли сообщение типа *message*, то мы пишем, что всё ок, иначе - отправляем это сообщение в другой топик (dead_letter).\nАвтор: Ниемисто Владимир, Nikel 2020")
 print("*"*50)
 
 try:
 	while 1:
+		"""
 		time.sleep(random.randint(0, 5))
 		vybor = (random.randint(1, 1000))%3
 		if vybor == 0:
@@ -17,15 +21,16 @@ try:
 				send_json = json.dumps({"type":"error"})
 			else:
 				send_json = json.dumps({"type":"nikel"})
-		
+		"""
+		for msg in consumer:
+			print(msg)
 
-		if (json.loads(send_json))['type'] == 'message':
-			print(datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S"),"\t",json.loads(send_json),"\tDone")
-			send_json = json.dumps({"type":"message"})
+			if (json.loads(msg))['type'] == 'message':
+				print(datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S"),"\tDone")
 
-		else:
-			print(datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S"),"\t",json.loads(send_json),"\t -> в другой топит топай!")
-			send_json = json.dumps({"type":"error"})
+			else:
+				print(datetime.datetime.utcnow().strftime("%Y.%m.%d %H:%M:%S"),"\t -> в другой топит топай!")
+
 		
 except KeyboardInterrupt:
 	print("\n")
