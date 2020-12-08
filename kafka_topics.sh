1228b5bdebce3e02cb2717f19138373dae49c22b
VladimirNikel <vova_niemisto@mail.ru>
2020-12-08 14:56:19 +0300

    fix запуска на сервере3_2

--- kafka_topics.sh
+++ kafka_topics.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-kafka-topics --create --topic main_topic --partitions 3 --replication-factor 3 --if-not-exists --zookeeper 194.61.2.84:5099
-kafka-topics --create --topic dead_letter --partitions 3 --replication-factor 3 --if-not-exists --zookeeper 194.61.2.84:5099
-kafka-topics --describe --topic main_topic --zookeeper 194.61.2.84:25099
+kafka-topics --create --topic main_topic --partitions 3 --replication-factor 3 --if-not-exists --zookeeper 194.61.2.84:2181
+kafka-topics --create --topic dead_letter --partitions 3 --replication-factor 3 --if-not-exists --zookeeper 194.61.2.84:2181
+kafka-topics --describe --topic main_topic --zookeeper 194.61.2.84:22181
 
 seq 1 | kafka-console-producer --broker-list 194.61.2.84:32770 --topic test && echo 'Produced 42 messages.'
 
